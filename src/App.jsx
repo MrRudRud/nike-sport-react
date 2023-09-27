37c768e58c2b4dcb0353a8906c043caec9a367c3
MrRudRud <mrrudrud@gmail.com>
2023-09-27 21:28:38 +0700

    master

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-black text-white text-4x1 font-bold">
+        Welcome to Nike Store Commerce Project
+      </h1>
+    </>
+  );
+};
+
+export default App;
