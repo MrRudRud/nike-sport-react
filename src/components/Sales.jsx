74881270b17bcfbbc34eb0757f797bef21089ff3
MrRudRud <mrrudrud@gmail.com>
2023-11-05 11:27:18 +0700

    adding new items

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,6 +1,21 @@
-const Sales = ({endpoint}) => {
-  console.log(endpoint);
-  return <div>Sales</div>;
+import Items from "./utils/Items";
+import Title from "./utils/Title";
+
+/* eslint-disable react/prop-types */
+const Sales = ({endpoint: {title, items}}) => {
+  // console.log({endpoint: {title, items}});
+  return (
+    <>
+      <div>
+        <Title title={title} />
+        <div>
+          {items.map((items, i) => (
+            <Items key={i} {...items} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sales;
--- src/components/utils/Items.jsx
+++ src/components/utils/Items.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import {ShoppingBagIcon, StarIcon} from "@heroicons/react/24/solid";
 
-const Items = () => {
-  return <div>Items</div>;
+const Items = ({id, title, text, rating, btn, img, price, color, shadow}) => {
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <p>{text}</p>
+        <div>
+          <div>
+            <h1>${price}</h1>
+          </div>
+          <div>
+            <StarIcon className="icon-style" />
+            <h1>{rating}</h1>
+          </div>
+        </div>
+        <button type="button">
+          <ShoppingBagIcon className="icon-style" />
+        </button>
+        <button type="button">{btn}</button>
+        {/* image content */}
+        <div>
+          <img src={img} alt={`img/item-img/${id}`} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Items;
